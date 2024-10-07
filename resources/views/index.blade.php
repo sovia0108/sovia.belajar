637c0a651acb8e0293b6b1f4ad4068ec45cfe68c
Sovia Lajuba <sovialajuba0108@gmail.com>
2024-10-07 15:34:29 +0700

    new

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -210,40 +210,17 @@ unggul di bidangnya </p>
 
           <div class="col-lg-7 d-flex flex-column justify-content-center order-2 order-lg-1">
 
-            <div class="content px-xl-5" data-aos="fade-up" data-aos-delay="100">
-              <h3><span>Eum ipsam laborum deleniti </span><strong>velit pariatur architecto aut nihil</strong></h3>
+            <div class="content px-xl-2" data-aos="fade-up" data-aos-delay="100">
+              <h3><span>PT Swabina Gatra</span> </h3 .
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit
+              PT Swabina Gatra atau yang lebih dikenal dengan nama SWA adalah perusahaan swasta nasional dan merupakan bagian dari Semen Indonesia Group. Berdiri sejak tahun 1988, SWA memiliki kisah sukses yang panjang dan terus tumbuh menjadi perusahaan modern, profesional dan bereputasi prima. SWA memiliki 5 lini bisnis utama yaitu Penyediaan Tenaga Kerja, produksi Air Minum Dalam Kemasan, Jasa Agen Perjalanan, Solusi Teknologi Informasi serta Lembaga Diklat & Sertifikasi.
               </p>
             </div>
+            
 
             <div class="faq-container px-xl-5" data-aos="fade-up" data-aos-delay="200">
 
-              <div class="faq-item faq-active">
-
-                <h3><span>01</span> Non consectetur a erat nam at lectus urna duis?</h3>
-                <div class="faq-content">
-                  <p>Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non.</p>
-                </div>
-                <i class="faq-toggle bi bi-chevron-right"></i>
-              </div><!-- End Faq item-->
-
-              <div class="faq-item">
-                <h3><span>02</span> Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque?</h3>
-                <div class="faq-content">
-                  <p>Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.</p>
-                </div>
-                <i class="faq-toggle bi bi-chevron-right"></i>
-              </div><!-- End Faq item-->
-
-              <div class="faq-item">
-                <h3><span>03</span> Dolor sit amet consectetur adipiscing elit pellentesque?</h3>
-                <div class="faq-content">
-                  <p>Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Faucibus pulvinar elementum integer enim. Sem nulla pharetra diam sit amet nisl suscipit. Rutrum tellus pellentesque eu tincidunt. Lectus urna duis convallis convallis tellus. Urna molestie at elementum eu facilisis sed odio morbi quis</p>
-                </div>
-                <i class="faq-toggle bi bi-chevron-right"></i>
-              </div><!-- End Faq item-->
-
+              
             </div>
 
           </div>
