a8d8cbfa1e7f1c0bad21d6fc31c0965ce41eaaa6
lisashii7 <alisa.qathrunnada.syaihu-2020@vokasi.unair.ac.id>
2024-10-08 14:43:58 +0700

    bug fix footer

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -897,83 +897,58 @@
   </main>
 
   <footer id="footer" class="footer">
-
-    <div class="footer-newsletter">
-      <div class="container">
-        <div class="row justify-content-center text-center">
-          <div class="col-lg-6">
-            <h4>Join Our Newsletter</h4>
-            <p>Subscribe to our newsletter and receive the latest news about our products and services!</p>
-            <form action="forms/newsletter.php" method="post" class="php-email-form">
-              <div class="newsletter-form"><input type="email" name="email"><input type="submit" value="Subscribe"></div>
-              <div class="loading">Loading</div>
-              <div class="error-message"></div>
-              <div class="sent-message">Your subscription request has been sent. Thank you!</div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-
     <div class="container footer-top">
       <div class="row gy-4">
-        <div class="col-lg-4 col-md-6 footer-about">
-          <a href="index.html" class="d-flex align-items-center">
-            <span class="sitename">Arsha</span>
-          </a>
-          <div class="footer-contact pt-3">
-            <p>A108 Adam Street</p>
-            <p>New York, NY 535022</p>
-            <p class="mt-3"><strong>Phone:</strong> <span>+1 5589 55488 55</span></p>
-            <p><strong>Email:</strong> <span>info@example.com</span></p>
+    
+        <!-- Bagian Tentang Perusahaan -->
+        <div class="col-lg-4 col-md-6 footer-about d-flex align-items-start">
+          <div class="why-us-img me-3">
+            <img src="assets/img/swabina.png" class="img-fluid" alt="PT Swabina Gatra" style="max-width: 80px;">
+          </div>
+          <div>
+            <h4 class="sitename">PT Swabina Gatra</h4>
+            <p>PT Swabina Gatra, atau SWA, adalah perusahaan swasta nasional yang bagian dari Semen Indonesia Group. Berdiri sejak 1988, SWA terus berkembang menjadi perusahaan modern dan profesional. SWA memiliki lima bidang utama: penyediaan tenaga kerja, produksi air minum kemasan, jasa agen perjalanan, solusi teknologi informasi, dan lembaga pelatihan serta sertifikasi.</p>
           </div>
         </div>
-
-        <div class="col-lg-2 col-md-3 footer-links">
-          <h4>Useful Links</h4>
-          <ul>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Home</a></li>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">About us</a></li>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Services</a></li>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Terms of service</a></li>
-          </ul>
+    
+        <!-- Bagian Informasi Kontak -->
+        <div class="col-lg-4 col-md-6 footer-contact">
+          <h4>Informasi Kontak</h4>
+          <p>
+            <i class="bi bi-geo-alt"></i> Kantor Pusat & Pabrik AMDK: <br> Jl. R.A. Kartini No.21 A Gresik 61122, <br> Jawa Timur
+          </p>
+          <div class="d-flex justify-content-between">
+            <p><i class="bi bi-envelope"></i> Email: Marketing@swabina.id</p>
+            <p><i class="bi bi-phone"></i> Phone: +62 31 12345678</p>
+          </div>
         </div>
-
-        <div class="col-lg-2 col-md-3 footer-links">
-          <h4>Our Services</h4>
-          <ul>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Web Design</a></li>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Web Development</a></li>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Product Management</a></li>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Marketing</a></li>
-          </ul>
-        </div>
-
+    
+        <!-- Bagian Stay Connected -->
         <div class="col-lg-4 col-md-12">
-          <h4>Follow Us</h4>
-          <p>Cras fermentum odio eu feugiat lide par naso tierra videa magna derita valies</p>
-          <div class="social-links d-flex">
-            <a href=""><i class="bi bi-twitter-x"></i></a>
-            <a href=""><i class="bi bi-facebook"></i></a>
-            <a href=""><i class="bi bi-instagram"></i></a>
-            <a href=""><i class="bi bi-linkedin"></i></a>
+          <h4>Stay Connected</h4>
+          <div class="social-links d-flex justify-content-start">
+            <a href="#" class="me-2"><i class="bi bi-instagram"></i></a>
+            <a href="#" class="me-2"><i class="bi bi-facebook"></i></a>
+            <a href="#" class="me-2"><i class="bi bi-twitter"></i></a>
+            <a href="#"><i class="bi bi-linkedin"></i></a>
           </div>
         </div>
-
+      </div>
+    
+      <!-- Stay Update With Us -->
+      <div class="row mt-4">
+        <div class="col-lg-6">
+          <h5>Stay Update With Us</h5>
+          <form action="#" method="post" class="d-flex">
+            <input type="email" class="form-control" placeholder="Enter your e-mail here">
+            <button type="submit" class="btn btn-primary ms-2 w-auto" style="padding: 0.375rem 1rem;">
+              <i class="bi bi-send"></i>
+            </button>
+          </form>
+        </div>
       </div>
     </div>
-
-    <div class="container copyright text-center mt-4">
-      <p>© <span>Copyright</span> <strong class="px-1 sitename">Arsha</strong> <span>All Rights Reserved</span></p>
-      <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you've purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-      </div>
-    </div>
-
+  
   </footer>
 
   <!-- Scroll Top -->
